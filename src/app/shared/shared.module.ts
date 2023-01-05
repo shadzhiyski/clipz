0faa6a8d44899ae754212e24049f523283550e20
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-05 18:02:53 +0200

    Register form validation added.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,24 +4,35 @@ import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
-
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent,
+    AlertComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe
   ],
   exports: [
     ModalComponent,
     TabComponent,
-    TabsContainerComponent
+    TabsContainerComponent,
+    InputComponent,
+    AlertComponent
   ],
   providers: [
-    ModalService
+    ModalService,
+    provideNgxMask()
   ]
 })
 export class SharedModule { }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl('',[
+      Validators.required,
+      Validators.min(18)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    ]),
+    confirm_password: new FormControl('',[
+      Validators.required
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(13),
+      Validators.maxLength(13)
+    ]),
+  });
 
+  showAlert = false
+  alertMsg?: string
+  alertColor?: string
+
+  constructor() {
+    this.setInitialAlertValues();
+  }
+
+  register() {
+    this.showAlert = true
+    this.alertMsg = 'Please wait! Your account is being created'
+    this.alertColor = 'blue'
+  }
+
+  setInitialAlertValues() {
+    this.alertMsg = 'Please wait! Your account is being created'
+    this.alertColor = 'blue'
+  }
 }
