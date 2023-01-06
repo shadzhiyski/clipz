8fffad1eb8c65d5ab728fd35c812d3e58694bb54
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-06 19:30:04 +0200

    user registration integrated with Firebase email authentication.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -37,15 +38,36 @@ export class RegisterComponent {
   showAlert = false
   alertMsg?: string
   alertColor?: string
+  inSubmission = false
 
-  constructor() {
+  constructor(private auth: AngularFireAuth) {
     this.setInitialAlertValues();
   }
 
-  register() {
+  async register() {
     this.showAlert = true
     this.alertMsg = 'Please wait! Your account is being created'
     this.alertColor = 'blue'
+    this.inSubmission = true
+
+    const { email, password} = this.registerForm.value
+
+
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(
+        email as string, password as string
+      )
+
+      console.log(userCred)
+    } catch (e) {
+      this.alertMsg = 'An unexpected error occurred. Please try again later'
+      this.alertColor = 'red'
+      this.inSubmission = false
+      return
+    }
+
+    this.alertMsg = 'Your account has been created'
+    this.alertColor = 'green'
   }
 
   setInitialAlertValues() {
