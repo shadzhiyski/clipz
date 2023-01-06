ff06f7d64488514ff7d39e6e9a8c5d6d4d75b5dd
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-06 21:18:55 +0200

    Login form integrated with Firebase email Authentication.

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  showAlert = false
+  alertMsg?: string
+  alertColor?: string
+  inSubmission = false
+
   credentials = {
     email: '',
     password: ''
   }
 
-  login() {
-    console.log(this.credentials)
+  constructor(private auth: AngularFireAuth) {
+    this.setInitialAlertValues()
+  }
+
+  async login() {
+    this.showAlert = true
+    this.setInitialAlertValues()
+    this.inSubmission = true
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email, this.credentials.password
+      )
+    } catch (e) {
+      this.inSubmission = false
+      this.alertMsg = 'An unexpected error occurred. Please try again later.'
+      this.alertColor = 'red'
+
+      console.log(e)
+      return
+    }
+
+    this.alertMsg = 'You\'re logged in.'
+    this.alertColor = 'green'
+  }
+
+  setInitialAlertValues() {
+    this.alertMsg = 'Please wait! We\'re logging you in.'
+    this.alertColor = 'blue'
   }
 }
