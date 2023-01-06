3bb1dd7c2efe947766dbc5673f53f6c92739b407
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-06 21:48:06 +0200

    Logout added.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
 
@@ -10,11 +11,18 @@ import { ModalService } from '../services/modal.service';
 export class NavComponent {
   constructor(
     public auth: AuthService,
-    public modal: ModalService) { }
+    public modal: ModalService,
+    private afAuth: AngularFireAuth) { }
 
   openModal(e: Event) {
     e.preventDefault() // prevent refresh from browser
 
     this.modal.toggleModal('auth');
+  }
+
+  async logout(e: Event) {
+    e.preventDefault()
+
+    await this.afAuth.signOut();
   }
 }
