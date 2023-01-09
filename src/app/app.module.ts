e8be3d1f84fda6fa28b6042b85e7e229f9d79729
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-09 16:15:07 +0200

    HomeComponent extracted and routing used.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { NavComponent } from './nav/nav.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
