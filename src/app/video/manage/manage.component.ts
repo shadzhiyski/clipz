ee88cb7bab001e3f16b848bcebaf256d1c94a2a2
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-11 09:09:25 +0200

    Clips listed on Mange page.

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { map } from 'rxjs';
+import IClip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-manage',
@@ -9,16 +11,30 @@ import { map } from 'rxjs';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1'
+  clips: IClip[] = []
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private clipService: ClipService) { }
 
   ngOnInit(): void {
     this.route.queryParamMap
       .pipe(map((params: Params) => params.params))
       .subscribe((params: Params) => {
         this.videoOrder = params.sort
+      })
+
+    this.clipService.getUserClips()
+      .subscribe(docs => {
+        this.clips = []
+
+        docs.forEach(doc => {
+          this.clips.push({
+            docId: doc.id,
+            ...doc.data()
+          })
+        })
       })
   }
 
