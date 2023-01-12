eea023ff2766e830155e7a03cd593ec79236addc
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-12 14:49:30 +0200

    Clip url copy tp clipboard implemented.

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -82,4 +82,17 @@ export class ManageComponent implements OnInit {
       }
     })
   }
+
+  async copyToClipboard(event: MouseEvent, docId: string | undefined) {
+    event.preventDefault()
+
+    if (!docId) {
+      return
+    }
+
+    const url = `${location.origin}/clip/${docId}`
+    await navigator.clipboard.writeText(url)
+
+    alert('Link Copied!')
+  }
 }
