9c0c7d3d8b803462eef7effd21b0fa53c5870a3b
Stoyan Hadzhiyski <c_shadzhiyski@capsuletech.com>
2023-01-12 14:59:33 +0200

    Lazy loading of VideoModule added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,7 @@ import { environment } from 'src/environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
-import { VideoModule } from './video/video.module';
+import { AboutComponent } from './about/about.component';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ClipsListComponent } from './clips-list/clips-list.component';
@@ -35,7 +34,6 @@ import { FbTimestampPipe } from './pipes/fb-timestamp.pipe';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    VideoModule,
     AppRoutingModule,
     AngularFireStorageModule
   ],
